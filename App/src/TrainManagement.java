3be63c74bedc5bc46902aeb06566455508290464
Tamizarasi-sathiya <tamizhsathiya07@gmail.com>
2026-04-08 15:47:37 +0530

    Tamizarasi Addded Sort Bogies by Capacity

--- App/src/TrainManagement.java
+++ App/src/TrainManagement.java
@@ -1,28 +1,37 @@
-import java.util.LinkedHashSet;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.*;
+
+class Bogie {
+    String name;
+    int capacity;
+
+    Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+}
 
 public class TrainManagement {
     public static void main(String[] args) {
-        LinkedHashSet<String> train = new LinkedHashSet<>();
         HashMap<String, Integer> bogieMap = new HashMap<>();
+        List<Bogie> list = new ArrayList<>();
 
-        train.add("Engine");
-        train.add("Sleeper");
-        train.add("Cargo");
-        train.add("Guard");
-        train.add("Sleeper");
+        list.add(new Bogie("Sleeper", 72));
+        list.add(new Bogie("AC Chair", 56));
+        list.add(new Bogie("First Class", 24));
+
         bogieMap.put("Sleeper", 72);
         bogieMap.put("AC Chair", 60);
         bogieMap.put("First Class", 24);
+        list.sort(Comparator.comparingInt(b -> b.capacity));
 
-        System.out.println("Train Formation:");
-        for (String bogie : train) {
-            System.out.print(bogie + " -> ");
-            System.out.println("Bogie Capacity Details:");
-            for (Map.Entry<String, Integer> entry : bogieMap.entrySet()) {
-                System.out.println(entry.getKey() + " : " + entry.getValue());
+        System.out.println("Bogie Capacity Details:");
+        for (Map.Entry<String, Integer> entry : bogieMap.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+            System.out.println("Bogies Sorted by Capacity:");
+            for (Bogie b : list) {
+                System.out.println(b.name + " : " + b.capacity);
             }
-            System.out.println("END");
         }
     }
