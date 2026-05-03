b62eca47dfa79c1ca8d59131b7e6add5d7af209b
Tamizarasi S <tamizhsathiya07@gmail.com>
2026-05-03 20:09:30 +0530

    Tamizarasi added UC14

--- App/src/Test/test.java
+++ App/src/Test/test.java
@@ -1,84 +1,33 @@
 package test;
 
+import main.Main.InvalidCapacityException;
+import main.Main.PassengerBogie;
 import org.junit.jupiter.api.Test;
-
-import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class TestCode {
+public class Test {
 
-    static class GoodsBogie {
-
-        String type;
-        String cargo;
-
-        GoodsBogie(String type, String cargo) {
-            this.type = type;
-            this.cargo = cargo;
-        }
-    }
-
-    // Method to check safety
-    boolean checkSafety(List<GoodsBogie> bogies) {
-
-        return bogies.stream()
-                .allMatch(b ->
-                        !b.type.equals("Cylindrical") ||
-                                b.cargo.equals("Petroleum")
-                );
+    @Test
+    void testValidCapacityCreation() throws InvalidCapacityException {
+        PassengerBogie b = new PassengerBogie("Sleeper", 50);
+        assertEquals("Sleeper", b.type);
+        assertEquals(50, b.capacity);
     }
 
     @Test
-    void testSafety_ValidPetroleumTransport() {
+    void testInvalidCapacityThrowsException() {
+        Exception exception = assertThrows(InvalidCapacityException.class, () -> {
+            new PassengerBogie("AC Chair", 0);
+        });
 
-        List<GoodsBogie> bogies = Arrays.asList(
-                new GoodsBogie("Cylindrical", "Petroleum"),
-                new GoodsBogie("Open", "Coal")
-        );
-
-        assertTrue(checkSafety(bogies));
+        assertEquals("Capacity must be greater than zero", exception.getMessage());
     }
 
     @Test
-    void testSafety_InvalidCylindricalCargo() {
-
-        List<GoodsBogie> bogies = Arrays.asList(
-                new GoodsBogie("Cylindrical", "Coal")
-        );
-
-        assertFalse(checkSafety(bogies));
-    }
-
-    @Test
-    void testSafety_MultipleValidBogies() {
-
-        List<GoodsBogie> bogies = Arrays.asList(
-                new GoodsBogie("Cylindrical", "Petroleum"),
-                new GoodsBogie("Box", "Grain"),
-                new GoodsBogie("Open", "Coal")
-        );
-
-        assertTrue(checkSafety(bogies));
-    }
-
-    @Test
-    void testSafety_EmptyList() {
-
-        List<GoodsBogie> bogies = new ArrayList<>();
-
-        assertTrue(checkSafety(bogies));
-    }
-
-    @Test
-    void testSafety_OriginalListUnchanged() {
-
-        List<GoodsBogie> bogies = new ArrayList<>();
-
-        bogies.add(new GoodsBogie("Open", "Coal"));
-
-        checkSafety(bogies);
-
-        assertEquals(1, bogies.size());
+    void testNegativeCapacityThrowsException() {
+        assertThrows(InvalidCapacityException.class, () -> {
+            new PassengerBogie("First Class", -10);
+        });
     }
 }
